49b3920c323e1234ceffc068068e2a931a81b8f3
387673116 <60752994+387673116@users.noreply.github.com>
2024-09-09 20:41:38 +0800

    1
    1

--- process_json.py
+++ process_json.py
@@ -31,7 +31,7 @@ if response.status_code == 200 and response.text.strip():
         pre_lives_content = cleaned_text.split('"lives":')[0]
 
         # 定义需要删除的关键字列表
-        keywords = ['虎牙直播', '有声小说吧', '88看球', '少儿', '小学', '初中', '墙外', '高中', '急救教学', '搜', '盘']
+        keywords = ['虎牙直播', '有声小说吧', '88看球', '少儿', '小学', '初中', 'TgYunPanLoca', '高中', '急救教学', '易搜', '网盘', '纸条搜', 'csp_PanSearch', '本地', '推送', '动漫']
 
         # 构建正则表达式，删除包含指定关键字的 {} 块及其后面的逗号
         for keyword in keywords:
